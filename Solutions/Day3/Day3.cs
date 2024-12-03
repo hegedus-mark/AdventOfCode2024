de21b56f5f74f48e86d36b2c34756c561e3e9310
hegedus-mark <hegedusmark2018@gmail.com>
2024-12-03 19:02:25 +0100

    Complete Day3 Part 2

--- Solutions/Day3/Day3.cs
+++ Solutions/Day3/Day3.cs
@@ -7,9 +7,9 @@ public class Day3
 {
 
     private readonly string _filePath;
-    private const string REGEX_PATTERN_TASK1 = @"mul\((\d+),(\d+)\)";
-    private const string REGEX_PATTERN_TASK2 = @"mul\((\d+),(\d+)\)";
-
+    private const string REGEX_MUL_PATTERN = @"mul\((\d+),(\d+)\)";
+    private const string REGEX_DODONT_BLOCK_PATTERN = @"do(?!n't)(.*?)(?=don't|$)";
+    private const string REGEX_START_TO_DONT_BLOCK_PATTERN = @"^(.*?)don't";
 
     public Day3(InputFilePathHelper inputFilePathHelper)
     {
@@ -18,31 +18,43 @@ public class Day3
     
     public int Part1()
     {
-        var result = 0;
 
         string fileContent = ReadInput();
 
-        MatchCollection matches = Regex.Matches(fileContent, REGEX_PATTERN_TASK1);
-
-        foreach (Match match in matches)
-        {
-            int num1 = int.Parse(match.Groups[1].Value);
-            int num2 = int.Parse(match.Groups[2].Value);
-
-            result += num1 * num2;
-        }
-
-        return result;
+        return FindAndMultiplyMulInBlock(fileContent);
     }
     
     public int Part2()
     {
-        var result = 0;
+        int result = 0;
         
         string fileContent = ReadInput();
 
-        MatchCollection matches = Regex.Matches(fileContent, REGEX_PATTERN_TASK2);
+        
+        var firstBlockRegex = new Regex(REGEX_START_TO_DONT_BLOCK_PATTERN, RegexOptions.Singleline);
+        var firstBlock = firstBlockRegex.Match(fileContent);
+        
+        
+        result += FindAndMultiplyMulInBlock(firstBlock.Value);
 
+        var remainingString = fileContent.Substring(firstBlock.Length);
+        
+        MatchCollection doDontBlocks = Regex.Matches(remainingString, REGEX_DODONT_BLOCK_PATTERN, RegexOptions.Singleline);
+
+        foreach (Match doDontBlock in doDontBlocks)
+        {
+            result += FindAndMultiplyMulInBlock(doDontBlock.Value);
+        }
+        
+
+        return result;
+    }
+
+    private int FindAndMultiplyMulInBlock(string block)
+    {
+        var result = 0;
+        MatchCollection matches = Regex.Matches(block, REGEX_MUL_PATTERN);
+        
         foreach (Match match in matches)
         {
             int num1 = int.Parse(match.Groups[1].Value);
@@ -53,9 +65,8 @@ public class Day3
 
         return result;
     }
-    
-    
 
+    
     private  string ReadInput()
     {
         string fileContent = File.ReadAllText(_filePath);
